424fb83ae6f6f8686f06e0f22e497bf61d029e75
Ling <linghypshen@gmail.com>
2022-03-29 08:23:25 -0400

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,68 +1,69 @@
 ### Docker Commands
 
 ```
-# docker [OPTIONS] COMMAND [arg...]
-#        docker daemon [ --help | ... ]
-#        docker [ --help | -v | --version ]
-# A
-# self-sufficient runtime for containers.
+docker [OPTIONS] COMMAND [arg...]
+       docker daemon [ --help | ... ]
+       docker [ --help | -v | --version ]
+A
+self-sufficient runtime for containers.
 
-# Options:
-#   --config=~/.docker              Location of client config files  #客户端配置文件的位置
-#   -D, --debug=false               Enable debug mode  #启用Debug调试模式
-#   -H, --host=[]                   Daemon socket(s) to connect to  #守护进程的套接字（Socket）连接
-#   -h, --help=false                Print usage  #打印使用
-#   -l, --log-level=info            Set the logging level  #设置日志级别
-#   --tls=false                     Use TLS; implied by--tlsverify  #
-#   --tlscacert=~/.docker/ca.pem    Trust certs signed only by this CA  #信任证书签名CA
-#   --tlscert=~/.docker/cert.pem    Path to TLS certificate file  #TLS证书文件路径
-#   --tlskey=~/.docker/key.pem      Path to TLS key file  #TLS密钥文件路径
-#   --tlsverify=false               Use TLS and verify the remote  #使用TLS验证远程
-#   -v, --version=false             Print version information and quit  #打印版本信息并退出
+Options:
+  --config=~/.docker              Location of client config files  #客户端配置文件的位置
+  -D, --debug=false               Enable debug mode  #启用Debug调试模式
+  -H, --host=[]                   Daemon socket(s) to connect to  #守护进程的套接字（Socket）连接
+  -h, --help=false                Print usage  #打印使用
+  -l, --log-level=info            Set the logging level  #设置日志级别
+  --tls=false                     Use TLS; implied by--tlsverify  #
+  --tlscacert=~/.docker/ca.pem    Trust certs signed only by this CA  #信任证书签名CA
+  --tlscert=~/.docker/cert.pem    Path to TLS certificate file  #TLS证书文件路径
+  --tlskey=~/.docker/key.pem      Path to TLS key file  #TLS密钥文件路径
+  --tlsverify=false               Use TLS and verify the remote  #使用TLS验证远程
+  -v, --version=false             Print version information and quit  #打印版本信息并退出
 
-# Commands:
-#     attach    Attach to a running container  #当前shell下attach连接指定运行镜像
-#     build     Build an image from a Dockerfile  #通过Dockerfile定制镜像
-#     commit    Create a new image from a container's changes  #提交当前容器为新的镜像
-#     cp    Copy files/folders from a container to a HOSTDIR or to STDOUT  #从容器中拷贝指定文件或者目录到宿主机中
-#     create    Create a new container  #创建一个新的容器，同run 但不启动容器
-#     diff    Inspect changes on a container's filesystem  #查看docker容器变化
-#     events    Get real time events from the server#从docker服务获取容器实时事件
-#     exec    Run a command in a running container#在已存在的容器上运行命令
-#     export    Export a container's filesystem as a tar archive  #导出容器的内容流作为一个tar归档文件(对应import)
-#     history    Show the history of an image  #展示一个镜像形成历史
-#     images    List images  #列出系统当前镜像
-#     import    Import the contents from a tarball to create a filesystem image  #从tar包中的内容创建一个新的文件系统映像(对应export)
-#     info    Display system-wide information  #显示系统相关信息
-#     inspect    Return low-level information on a container or image  #查看容器详细信息
-#     kill    Kill a running container  #kill指定docker容器
-#     load    Load an image from a tar archive or STDIN  #从一个tar包中加载一个镜像(对应save)
-#     login    Register or log in to a Docker registry#注册或者登陆一个docker源服务器
-#     logout    Log out from a Docker registry  #从当前Docker registry退出
-#     logs    Fetch the logs of a container  #输出当前容器日志信息
-#     pause    Pause all processes within a container#暂停容器
-#     port    List port mappings or a specific mapping for the CONTAINER  #查看映射端口对应的容器内部源端口
-#     ps    List containers  #列出容器列表
-#     pull    Pull an image or a repository from a registry  #从docker镜像源服务器拉取指定镜像或者库镜像
-#     push    Push an image or a repository to a registry  #推送指定镜像或者库镜像至docker源服务器
-#     rename    Rename a container  #重命名容器
-#     restart    Restart a running container  #重启运行的容器
-#     rm    Remove one or more containers  #移除一个或者多个容器
-#     rmi    Remove one or more images  #移除一个或多个镜像(无容器使用该镜像才可以删除，否则需要删除相关容器才可以继续或者-f强制删除)
-#     run    Run a command in a new container  #创建一个新的容器并运行一个命令
-#     save    Save an image(s) to a tar archive#保存一个镜像为一个tar包(对应load)
-#     search    Search the Docker Hub for images  #在docker # hub中搜索镜像
-#     start    Start one or more stopped containers#启动容器
-#     stats    Display a live stream of container(s) resource usage statistics  #统计容器使用资源
-#     stop    Stop a running container  #停止容器
-#     tag         Tag an image into a repository  #给源中镜像打标签
-#     top       Display the running processes of a container #查看容器中运行的进程信息
-#     unpause    Unpause all processes within a container  #取消暂停容器
-#     version    Show the Docker version information#查看容器版本号
-#     wait         Block until a container stops, then print its exit code  #截取容器停止时的退出状态值
-```
+Commands:
+    attach    Attach to a running container  #当前shell下attach连接指定运行镜像
+    build     Build an image from a Dockerfile  #通过Dockerfile定制镜像
+    commit    Create a new image from a container's changes  #提交当前容器为新的镜像
+    cp    Copy files/folders from a container to a HOSTDIR or to STDOUT  #从容器中拷贝指定文件或者目录到宿主机中
+    create    Create a new container  #创建一个新的容器，同run 但不启动容器
+    diff    Inspect changes on a container's filesystem  #查看docker容器变化
+    events    Get real time events from the server#从docker服务获取容器实时事件
+    exec    Run a command in a running container#在已存在的容器上运行命令
+    export    Export a container's filesystem as a tar archive  #导出容器的内容流作为一个tar归档文件(对应import)
+    history    Show the history of an image  #展示一个镜像形成历史
+    images    List images  #列出系统当前镜像
+    import    Import the contents from a tarball to create a filesystem image  #从tar包中的内容创建一个新的文件系统映像(对应export)
+    info    Display system-wide information  #显示系统相关信息
+    inspect    Return low-level information on a container or image  #查看容器详细信息
+    kill    Kill a running container  #kill指定docker容器
+    load    Load an image from a tar archive or STDIN  #从一个tar包中加载一个镜像(对应save)
+    login    Register or log in to a Docker registry#注册或者登陆一个docker源服务器
+    logout    Log out from a Docker registry  #从当前Docker registry退出
+    logs    Fetch the logs of a container  #输出当前容器日志信息
+    pause    Pause all processes within a container#暂停容器
+    port    List port mappings or a specific mapping for the CONTAINER  #查看映射端口对应的容器内部源端口
+    ps    List containers  #列出容器列表
+    pull    Pull an image or a repository from a registry  #从docker镜像源服务器拉取指定镜像或者库镜像
+    push    Push an image or a repository to a registry  #推送指定镜像或者库镜像至docker源服务器
+    rename    Rename a container  #重命名容器
+    restart    Restart a running container  #重启运行的容器
+    rm    Remove one or more containers  #移除一个或者多个容器
+    rmi    Remove one or more images  #移除一个或多个镜像(无容器使用该镜像才可以删除，否则需要删除相关容器才可以继续或者-f强制删除)
+    run    Run a command in a new container  #创建一个新的容器并运行一个命令
+    save    Save an image(s) to a tar archive#保存一个镜像为一个tar包(对应load)
+    search    Search the Docker Hub for images  #在docker # hub中搜索镜像
+    start    Start one or more stopped containers#启动容器
+    stats    Display a live stream of container(s) resource usage statistics  #统计容器使用资源
+    stop    Stop a running container  #停止容器
+    tag         Tag an image into a repository  #给源中镜像打标签
+    top       Display the running processes of a container #查看容器中运行的进程信息
+    unpause    Unpause all processes within a container  #取消暂停容器
+    version    Show the Docker version information#查看容器版本号
+    wait         Block until a container stops, then print its exit code  #截取容器停止时的退出状态值
+	
 Run 'docker COMMAND --help' for more information on a command.  
 运行docker命令在帮助可以获取更多信息
+```
 
 ### Docker command examples
 
@@ -128,7 +129,7 @@ docker --help
 ```
 - docker run
 ```
-# 后台运行一个docker
+后台运行一个docker
 docker run -d centos /bin/sh -c "while true;do echo 正在运行; sleep 1;done"
     # -d  后台运行容器
     # /bin/sh  指定使用centos的bash解释器
@@ -168,7 +169,6 @@ ee9
 [root@ee92fcf6f32d /]#   # 注意看用户名，已经变成容器用户名
 ```
 - docker run image
-
 ```
 1.我们进入交互式的centos容器中，发现没有vim命令
     docker run -it centos
@@ -289,18 +289,18 @@ ENTRYPOINT ["/bin/bash","-c","echo hello $name”]
 
 - docker hub
 ```
-# 注册docker id后，在linux中登录dockerhub
+注册docker id后，在linux中登录dockerhub
     docker login
 
-# 注意要保证image的tag是账户名，如果镜像名字不对，需要改一下tag
+注意要保证image的tag是账户名，如果镜像名字不对，需要改一下tag
     docker tag chaoyu/centos-vim ling104/centos-vim
     # 语法是：docker tag   仓库名   ling104/仓库名
 
-# 推送docker image到dockerhub
+推送docker image到dockerhub
     docker push ling104/centps-cmd-exec:latest
 
-# 去dockerhub中检查镜像
-# 先删除本地镜像，然后再测试下载pull 镜像文件
+去dockerhub中检查镜像
+先删除本地镜像，然后再测试下载pull 镜像文件
     docker pull ling104/centos-entrypoint-exec
 ```
 
